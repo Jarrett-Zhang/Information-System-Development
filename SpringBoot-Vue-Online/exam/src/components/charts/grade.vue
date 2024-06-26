1647cb9a0a1c8a076fdbac5cfeb6e4890816f132
zsf37 <2074884581@qq.com>
2024-06-26 23:09:29 +0800

    Create grade.vue

---
+++ SpringBoot-Vue-Online/exam/src/components/charts/grade.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="grade">
+    <div ref="box" class="box"></div>
+    <div class="notFound" v-if="isNull">
+      <i class="iconfont icon-LC_icon_tips_fill"></i><span>该考生未参加考试</span>
+    </div>
+  </div>
+</template>
+
+
+<style lang="less" scoped>
+#grade {
+  position: relative;
+  .box{
+    width: 600px;
+    height: 400px;
+  }
+  .notFound {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+  }
+}
+</style>
