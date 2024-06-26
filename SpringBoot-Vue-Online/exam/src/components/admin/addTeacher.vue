8629e9ba16563a692abac2ac16ebbbc5b92b40c6
zsf37 <2074884581@qq.com>
2024-06-26 19:41:30 +0800

    Create addTeacher.vue

---
+++ SpringBoot-Vue-Online/exam/src/components/admin/addTeacher.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="add">
+    <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="姓名">
+            <el-input v-model="form.teacherName"></el-input>
+          </el-form-item>
+          <el-form-item label="学院">
+            <el-input v-model="form.institute"></el-input>
+          </el-form-item>
+          <el-form-item label="性别">
+            <el-input v-model="form.sex"></el-input>
+          </el-form-item>
+          <el-form-item label="电话号码">
+            <el-input v-model="form.tel"></el-input>
+          </el-form-item>
+          <el-form-item label="密码">
+            <el-input v-model="form.pwd"></el-input>
+          </el-form-item>
+          <el-form-item label="身份证号">
+            <el-input v-model="form.cardId"></el-input>
+          </el-form-item>
+          <el-form-item label="职称">
+            <el-input v-model="form.type"></el-input>
+          </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit()">立即创建</el-button>
+        <el-button type="text" @click="cancel()">取消</el-button>
+      </el-form-item>
+    </el-form>
+  </section>
+</template>
+<style lang="less" scoped>
+.add {
+  padding: 0px 40px;
+  width: 400px;
+}
+</style>
