090a9a6418ee2eda70f360d826b63f0929fa149f
zsf37 <2074884581@qq.com>
2024-06-26 23:10:51 +0800

    Create scorePart.vue

---
+++ SpringBoot-Vue-Online/exam/src/components/charts/scorePart.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="part" >
+    <div class="box" ref="box"></div>
+    <div v-if="isNull">
+      <span>该门考试还没人参考哦,请提醒学生参加考试。</span>
+    </div>
+  </div>
+</template>
+
+
+<style lang="less" scoped>
+.part {
+  .box {
+    width: 800px;
+    height: 800px;
+    margin-left: 40px;
+  }
+}
+</style>
