891c8713fa450dcff58ac7f684e9030955bef584
zsf37 <2074884581@qq.com>
2024-06-28 20:37:22 +0800

    Create studentManage.vue

---
+++ SpringBoot-Vue-Online/exam/src/components/teacher/studentManage.vue
@@ -0,0 +1,92 @@
+// 学生管理页面
+<template>
+  <div class="all">
+    <el-table :data="pagination.records" border>
+      <el-table-column fixed="left" prop="studentName" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="institute" label="学院" width="200"></el-table-column>
+      <el-table-column prop="major" label="专业" width="200"></el-table-column>
+      <el-table-column prop="grade" label="年级" width="200"></el-table-column>
+      <el-table-column prop="clazz" label="班级" width="100"></el-table-column>
+      <el-table-column prop="sex" label="性别" width="120"></el-table-column>
+      <el-table-column prop="tel" label="联系方式" width="120"></el-table-column>
+      <el-table-column fixed="right" label="操作" width="150">
+        <template slot-scope="scope">
+          <el-button @click="checkGrade(scope.row.studentId)" type="primary" size="small">编辑</el-button>
+          <el-button @click="deleteById(scope.row.studentId)" type="danger" size="small">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="pagination.current"
+      :page-sizes="[6, 10]"
+      :page-size="pagination.size"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="pagination.total"
+      class="page">
+    </el-pagination>
+    <!-- 编辑对话框-->
+    <el-dialog
+      title="编辑试卷信息"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :before-close="handleClose">
+      <section class="update">
+        <el-form ref="form" :model="form" label-width="80px">
+          <el-form-item label="姓名">
+            <el-input v-model="form.studentName"></el-input>
+          </el-form-item>
+          <el-form-item label="学院">
+            <el-input v-model="form.institute"></el-input>
+          </el-form-item>
+          <el-form-item label="专业">
+            <el-input v-model="form.major"></el-input>
+          </el-form-item>
+          <el-form-item label="年级">
+            <el-input v-model="form.grade"></el-input>
+          </el-form-item>
+          <el-form-item label="班级">
+            <el-input v-model="form.clazz"></el-input>
+          </el-form-item>
+          <el-form-item label="性别">
+            <el-input v-model="form.sex"></el-input>
+          </el-form-item>
+          <el-form-item label="电话号码">
+            <el-input v-model="form.tel"></el-input>
+          </el-form-item>
+        </el-form>
+      </section>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submit()">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+
+<style lang="less" scoped>
+.all {
+  padding: 0px 40px;
+  .page {
+    margin-top: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .edit {
+    margin-left: 20px;
+  }
+  .el-table tr {
+    background-color: #dd5862 !important;
+  }
+}
+.el-table .warning-row {
+  background: #000 !important;
+}
+
+.el-table .success-row {
+  background: #dd5862;
+}
+</style>
