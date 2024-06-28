ac916aed97a7612e688e02f92437c49eaad1c18f
zsf37 <2074884581@qq.com>
2024-06-28 20:32:07 +0800

    Create selectExamToPart.vue

---
+++ SpringBoot-Vue-Online/exam/src/components/teacher/selectExamToPart.vue
@@ -0,0 +1,47 @@
+//查询所有考试跳转到分段页面
+<template>
+  <div class="exam">
+    <el-table :data="pagination.records" border>
+      <el-table-column fixed="left" prop="source" label="试卷名称" width="180"></el-table-column>
+      <el-table-column prop="description" label="介绍" width="200"></el-table-column>
+      <el-table-column prop="institute" label="所属学院" width="120"></el-table-column>
+      <el-table-column prop="major" label="所属专业" width="200"></el-table-column>
+      <el-table-column prop="grade" label="年级" width="100"></el-table-column>
+      <el-table-column prop="examDate" label="考试日期" width="120"></el-table-column>
+      <el-table-column prop="totalTime" label="持续时间" width="120"></el-table-column>
+      <el-table-column prop="totalScore" label="总分" width="120"></el-table-column>
+      <el-table-column prop="type" label="试卷类型" width="120"></el-table-column>
+      <el-table-column prop="tips" label="考生提示" width="400"></el-table-column>
+      <el-table-column fixed="right" label="操作" width="150">
+        <template slot-scope="scope">
+          <el-button @click="toPart(scope.row.examCode,scope.row.source)" type="primary" size="small">查看分段</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="pagination.current"
+      :page-sizes="[4, 8, 10, 20]"
+      :page-size="pagination.size"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="pagination.total" class="page">
+    </el-pagination>
+  </div>
+</template>
+
+
+<style lang="less" scoped>
+.exam {
+  padding: 0px 40px;
+  .page {
+    margin-top: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .edit{
+    margin-left: 20px;
+  }
+}
+</style>
