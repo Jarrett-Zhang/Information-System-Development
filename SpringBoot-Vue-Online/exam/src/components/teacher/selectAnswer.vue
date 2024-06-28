9b877e6b4395ac5d54d27969c63641462c3c0eef
zsf37 <2074884581@qq.com>
2024-06-28 20:29:23 +0800

    Create selectAnswer.vue

---
+++ SpringBoot-Vue-Online/exam/src/components/teacher/selectAnswer.vue
@@ -0,0 +1,51 @@
+//查询所有题库
+<template>
+  <div class="exam">
+    <el-table :data="pagination.records" border :row-class-name="tableRowClassName">
+      <el-table-column fixed="left" prop="subject" label="试卷名称" width="180"></el-table-column>
+      <el-table-column prop="question" label="题目信息" width="490"></el-table-column>
+      <el-table-column prop="section" label="所属章节" width="200"></el-table-column>
+      <el-table-column prop="type" label="题目类型" width="200"></el-table-column>
+      <el-table-column prop="score" label="试题分数" width="150"></el-table-column>
+      <el-table-column prop="level" label="难度等级" width="133"></el-table-column>
+    </el-table>
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="pagination.current"
+      :page-sizes="[6, 10]"
+      :page-size="pagination.size"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="pagination.total"
+      class="page"
+    ></el-pagination>
+  </div>
+</template>
+
+
+<style lang="less" scoped>
+.exam {
+  padding: 0px 40px;
+  .page {
+    margin-top: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .edit {
+    margin-left: 20px;
+  }
+  .el-table tr {
+    background-color: #DD5862 !important;
+  }
+}
+  .el-table .warning-row {
+    background: #000 !important;
+    
+  }
+
+  .el-table .success-row {
+    background: #DD5862;
+  }
+
+</style>
