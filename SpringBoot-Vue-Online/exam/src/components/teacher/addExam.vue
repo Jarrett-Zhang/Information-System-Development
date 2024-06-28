4b83980d2e61e8e94a32f5063c07d155bb0a3074
zsf37 <2074884581@qq.com>
2024-06-28 20:15:28 +0800

    Create addExam.vue

---
+++ SpringBoot-Vue-Online/exam/src/components/teacher/addExam.vue
@@ -0,0 +1,51 @@
+<!-- 添加考试 -->
+<template>
+  <section class="add">
+    <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="试卷名称">
+        <el-input v-model="form.source"></el-input>
+      </el-form-item>
+      <el-form-item label="介绍">
+        <el-input v-model="form.description"></el-input>
+      </el-form-item>
+      <el-form-item label="所属学院">
+        <el-input v-model="form.institute"></el-input>
+      </el-form-item>
+      <el-form-item label="所属专业">
+        <el-input v-model="form.major"></el-input>
+      </el-form-item>
+      <el-form-item label="年级">
+        <el-input v-model="form.grade"></el-input>
+      </el-form-item>
+      <el-form-item label="考试日期">
+        <el-col :span="11">
+          <el-date-picker placeholder="选择日期" v-model="form.examDate" style="width: 100%;"></el-date-picker>
+        </el-col>
+      </el-form-item>
+      <el-form-item label="持续时间">
+        <el-input v-model="form.totalTime"></el-input>
+      </el-form-item>
+      <el-form-item label="总分">
+        <el-input v-model="form.totalScore"></el-input>
+      </el-form-item>
+      <el-form-item label="考试类型">
+        <el-input v-model="form.type"></el-input>
+      </el-form-item>
+      <el-form-item label="考生提示">
+        <el-input type="textarea" v-model="form.tips"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit()">立即创建</el-button>
+        <el-button type="text" @click="cancel()">取消</el-button>
+      </el-form-item>
+    </el-form>
+  </section>
+</template>
+
+
+<style lang="less" scoped>
+.add {
+  padding: 0px 40px;
+  width: 400px;
+}
+</style>
