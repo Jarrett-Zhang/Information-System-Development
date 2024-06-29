161357b702e60be246494d5bb250dad566e3c17e
YeMo0001 <163085501+YeMo0001@users.noreply.github.com>
2024-06-29 17:52:22 +0800

    Update grade.vue

--- SpringBoot-Vue-Online/exam/src/components/charts/grade.vue
+++ SpringBoot-Vue-Online/exam/src/components/charts/grade.vue
@@ -7,7 +7,60 @@
   </div>
 </template>
 
-
+<script>
+export default {
+  name: "grade",
+  data() {
+    return {
+      isNull: false, //原始数据
+      tableDataX: [], //x轴数据 保存次数
+      tableDataY: [], //y轴数据 保存分数
+    }
+  },
+  mounted() {
+    this.score();
+  },
+  methods: {
+    score() {
+      let studentId = this.$route.query.studentId
+      this.$axios(`/api/score/${studentId}`).then(res => { //根据学生Id查询成绩
+        console.log(res)
+        if(res.data.code == 200) {
+          let rootData = res.data.data
+          rootData.forEach((element,index) => {
+            this.tableDataX.push(`第${index + 1}次`)
+            this.tableDataY.push(element.etScore)
+          });
+          let boxDom = this.$refs["box"];
+          let scoreCharts = this.$echarts.init(boxDom);
+          let option = {
+            xAxis: {
+              type: "category",
+              data: this.tableDataX
+            },
+            yAxis: {
+              type: "value"
+            },
+            series: [
+              {
+                data: this.tableDataY,
+                type: "line",
+                itemStyle: { normal: { label: { show: true } } }
+              }
+            ]
+          };
+          scoreCharts.setOption(option);
+          scoreCharts.on("mouseover", params => {
+            console.log(params.value);
+          });
+        }else {
+          this.isNull = true
+        }
+      })
+    }
+  }
+};
+</script>
 <style lang="less" scoped>
 #grade {
   position: relative;
